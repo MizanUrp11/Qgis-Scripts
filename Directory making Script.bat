9cf5c41546f8318b233e6efb0e305001410ed493
MizanUrp11 <mrn1105009@gmail.com>
2021-08-17 12:51:32 +0600

    1.0.1

---
+++ Directory making Script.bat
@@ -0,0 +1,3 @@
+SET mydir=%cd%
+cd %mydir%
+md "Scanned Images" "Shapefile" "Freehand" "Webmap" "GCPs" "Shapefile/Angle Points" "Shapefile/Existing Pipeline" "Shapefile/Georeferenced Mauja" "Shapefile/Proposed Pipeline"
