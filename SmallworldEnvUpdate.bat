85589a3e6598787e81d8c394018e156c3c6456b9
MizanUrp11 <mrn1105009@gmail.com>
2022-01-07 23:50:37 +0600

    7zip support and bug fixes

--- SmallworldEnvUpdate.bat
+++ SmallworldEnvUpdate.bat
@@ -1,3 +1,11 @@
+set PATH=%PATH%;C:\Program Files\7-Zip\
+for /f %%I in ('dir /b /s *.zip') do (set myvar="%%I")
+for /f %%I in ('dir /b /s *.zip') do (7z x -o"%%~dpI" "%%I")
+if exist %myvar% del %myvar%
+for /f %%I in ('dir /b /s *.zip') do (7z x -o"%%~dpI" "%%I")
+for /f %%I in ('dir /b /s *.zip') do (del %%I)
+
+
 rmdir /Q /S C:\SW_Source\bangladesh430
 xcopy D:\latest_env\bangladesh430 C:\SW_Source\bangladesh430 /E/H/C/I
 
@@ -34,5 +42,4 @@ xcopy D:\latest_env\ds\ds_tgtdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_tgtdcl /
 xcopy D:\latest_env\ds\ds_winnap C:\SW_Data\bangladesh\bangladesh\ds\ds_winnap /E/H/C/I
 xcopy D:\latest_env\ds\ds_wn_kgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_wn_kgdcl /E/H/C/I
 del /f/s/q "C:\SW_Source\images" > nul
-cd C:\SW_Source\bangladesh430\bangladesh\scripts
-emacs_bangla430_sa.bat
+call c:\SW_Source\bangladesh430\bangladesh\scripts\emacs_bangla430_sa.bat
