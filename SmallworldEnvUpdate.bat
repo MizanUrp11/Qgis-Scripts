81fbd911d2b9f67e45f3b91e220b5c074416e0f8
MizanUrp11 <mrn1105009@gmail.com>
2022-01-12 14:22:44 +0600

    Bug Fixes

--- SmallworldEnvUpdate.bat
+++ SmallworldEnvUpdate.bat
@@ -13,13 +13,13 @@ for /f %%I in ('dir /b /s *.zip') do (del %%I)
 
 
 rmdir /Q /S C:\SW_Source\bangladesh430
-xcopy D:\latest_env\bangladesh430 C:\SW_Source\bangladesh430 /E/H/C/I
+xcopy %cd%\bangladesh430 C:\SW_Source\bangladesh430 /E/H/C/I
 
 rmdir /Q /S C:\SW_Source\gp_prd430
-xcopy D:\latest_env\gp_prd430 C:\SW_Source\gp_prd430 /E/H/C/I
+xcopy %cd%\gp_prd430 C:\SW_Source\gp_prd430 /E/H/C/I
 
 rmdir /Q /S C:\SW_Data\bangladesh\bangladesh\config
-xcopy D:\latest_env\config C:\SW_Data\bangladesh\bangladesh\config /E/H/C/I
+xcopy %cd%\config C:\SW_Data\bangladesh\bangladesh\config /E/H/C/I
 
 rmdir /Q /S C:\SW_Data\bangladesh\bangladesh\ds\ds_admin
 rmdir /Q /S C:\SW_Data\bangladesh\bangladesh\ds\ds_bgdcl
@@ -35,18 +35,18 @@ rmdir /Q /S C:\SW_Data\bangladesh\bangladesh\ds\ds_winnap
 rmdir /Q /S C:\SW_Data\bangladesh\bangladesh\ds\ds_wn_kgdcl
 
 
-xcopy D:\latest_env\ds\ds_admin C:\SW_Data\bangladesh\bangladesh\ds\ds_admin /E/H/C/I
-xcopy D:\latest_env\ds\ds_bgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_bgdcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_gis C:\SW_Data\bangladesh\bangladesh\ds\ds_gis /E/H/C/I
-xcopy D:\latest_env\ds\ds_jgtdsl C:\SW_Data\bangladesh\bangladesh\ds\ds_jgtdsl /E/H/C/I
-xcopy D:\latest_env\ds\ds_kgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_kgdcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_pgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_pgcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_rpgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_rpgcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_sgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_sgcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_survey C:\SW_Data\bangladesh\bangladesh\ds\ds_survey /E/H/C/I
-xcopy D:\latest_env\ds\ds_tgtdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_tgtdcl /E/H/C/I
-xcopy D:\latest_env\ds\ds_winnap C:\SW_Data\bangladesh\bangladesh\ds\ds_winnap /E/H/C/I
-xcopy D:\latest_env\ds\ds_wn_kgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_wn_kgdcl /E/H/C/I
+xcopy %cd%\ds\ds_admin C:\SW_Data\bangladesh\bangladesh\ds\ds_admin /E/H/C/I
+xcopy %cd%\ds\ds_bgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_bgdcl /E/H/C/I
+xcopy %cd%\ds\ds_gis C:\SW_Data\bangladesh\bangladesh\ds\ds_gis /E/H/C/I
+xcopy %cd%\ds\ds_jgtdsl C:\SW_Data\bangladesh\bangladesh\ds\ds_jgtdsl /E/H/C/I
+xcopy %cd%\ds\ds_kgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_kgdcl /E/H/C/I
+xcopy %cd%\ds\ds_pgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_pgcl /E/H/C/I
+xcopy %cd%\ds\ds_rpgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_rpgcl /E/H/C/I
+xcopy %cd%\ds\ds_sgcl C:\SW_Data\bangladesh\bangladesh\ds\ds_sgcl /E/H/C/I
+xcopy %cd%\ds\ds_survey C:\SW_Data\bangladesh\bangladesh\ds\ds_survey /E/H/C/I
+xcopy %cd%\ds\ds_tgtdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_tgtdcl /E/H/C/I
+xcopy %cd%\ds\ds_winnap C:\SW_Data\bangladesh\bangladesh\ds\ds_winnap /E/H/C/I
+xcopy %cd%\ds\ds_wn_kgdcl C:\SW_Data\bangladesh\bangladesh\ds\ds_wn_kgdcl /E/H/C/I
 del /f/s/q "C:\SW_Source\images" > nul
 
 call c:\SW_Source\bangladesh430\bangladesh\scripts\emacs_bangla430_sa.bat
